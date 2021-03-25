6113751dd8a042ddd4c247be0cd78b2f17856121
Simon Chaykin <52384223+thecakeisfalse@users.noreply.github.com>
2021-03-25 19:17:17 +0000

    Update app

--- Lic1575_Forum/main.py
+++ Lic1575_Forum/main.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import (
+    Flask, render_template, request, session, redirect, url_for
+)
+
+import secrets
 
 app = Flask(__name__)
+app.secret_key = secrets.token_urlsafe(32)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/profile')
+def profile():
+    return 'Profile'
+
+@app.route('/rules')
+def rules():
+    return 'Rules'
+
 @app.errorhandler(404)
 def not_found(error):
-    print(error)
     return render_template('404.html'), 404
 
-app.run()
+app.run(debug=True)
